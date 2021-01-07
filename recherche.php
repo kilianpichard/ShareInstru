8d8878845eee4181b41a01e301554a156f37b497
Kilian Pichard <48067485+PichardKil@users.noreply.github.com>
2021-01-07 15:45:27 +0100

    Fix DS_STORE and other ignored files

--- recherche.php
+++ recherche.php
@@ -1,18 +1,15 @@
 <?php
-include("pdo_oracle.php");
+
+  include("pdo_oracle.php");
+
   function Connexion(){
     $login = 'instruments';
     $mdp = 'Esha2ohCheu5eij3';
-
     $db = 'mysql:host=localhost;dbname=instruments_bd';
     return OuvrirConnexionPDO($db,$login,$mdp);
   }
 
-  if($_REQUEST["function"]=="cuivres"){
-    getCuivre();
-  }else if($_REQUEST["function"]=="new"){
-    getNew();
-  }
+  get();
 
   function sql($req){
     $conn = Connexion();
@@ -21,26 +18,20 @@ include("pdo_oracle.php");
     return $tab;
   }
 
-function getCuivre(){
-  $req = 'SELECT MARQUE.MAR_NOM, UTILISATEUR.UTI_NOM,UTILISATEUR.UTI_PRENOM,VILLE.VIL_NOM, INS_NOM, INS_DESCRIPTION,INS_PHOTO,UTI_PHOTO FROM `INSTRUMENT` as instru
-join MARQUE using (MAR_NUMERO)
-join VILLE using (VIL_NUMERO)
-join UTILISATEUR USING (UTI_NUMERO)
-join CATEGORIE using (CAT_NUMERO)';
-  $new = sql($req);
-  for ($i=0;$i<sizeof($new);$i++){
-    echo($new[$i]["MAR_NOM"].'|'.$new[$i]["UTI_NOM"].'|'.$new[$i]["UTI_PRENOM"].'|'.$new[$i]["VIL_NOM"].'|'.$new[$i]["INS_NOM"]
-      .'|'.utf8_encode($new[$i]["INS_DESCRIPTION"]).'|'.$new[$i]["INS_PHOTO"].'|'.$new[$i]["UTI_PHOTO"].'/////');
-  }
-
+function get(){
+  $req = 'SELECT MARQUE.MAR_NOM, UTILISATEUR.UTI_NOM,UTILISATEUR.UTI_PRENOM,VILLE.VIL_NOM, INS_NOM, INS_DESCRIPTION,INS_PHOTO,UTI_PHOTO,CAT_FAMILLE FROM `INSTRUMENT` as instru
+  join MARQUE using (MAR_NUMERO)
+  join VILLE using (VIL_NUMERO)
+  join UTILISATEUR USING (UTI_NUMERO)
+  join CATEGORIE using (CAT_NUMERO)';
+  send($req);
 }
 
-function remplirOption($tab)
-{
-  for ($i=0;$i<sizeof($tab);$i++)
-  {
-    echo '<option value="'.utf8_encode($tab[$i]["cat_nom"]).'">'.utf8_encode($tab[$i]['cat_nom']);
-    echo '</option>';
+  function send($req){
+    $new = sql($req);
+    for ($i=0;$i<sizeof($new);$i++){
+      echo($new[$i]["MAR_NOM"].'|'.$new[$i]["UTI_NOM"].'|'.$new[$i]["UTI_PRENOM"].'|'.$new[$i]["VIL_NOM"].'|'.$new[$i]["INS_NOM"]
+        .'|'.utf8_encode($new[$i]["INS_DESCRIPTION"]).'|'.$new[$i]["INS_PHOTO"].'|'.$new[$i]["UTI_PHOTO"].'|'.$new[$i]["CAT_FAMILLE"].'//||//');
+    }
   }
-}
 ?>
