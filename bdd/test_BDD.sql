32b761aa777298f5301a13179f890de9325458a3
marieeonnet <eonnet.marie2@gmail.com>
2021-01-10 17:31:56 +0100

    ajout de nouvelles données à la base et ecriture de nouvelles requêtes de tests

--- bdd/test_BDD.sql
+++ bdd/test_BDD.sql
@@ -3,7 +3,7 @@
 --Table Instrument--
 
 --Test 1: Instrument disponible--
-select * from Instrument where ins_dipos = "oui"; 
+select * from Instrument where ins_disponible = "oui"; 
 
 -- Test 2: Jointure Instrument et Marque + nom Marque--
 select * from Instrument join marque using(mar_numero); 
@@ -22,7 +22,7 @@ select * from Instrument join Utilisateur using(uti_numero);
 select * from Reserver join Utilisateur using(uti_numero) join Instrument using(ins_numero);
 
 --Test 6: Instrument représenté par une image--
-select * from representer joi Instrument using(ins_numero) join image(ima_num);
+select * from representer join Instrument using(ins_numero) join image(ima_num);
 
 -----------------------------------------------------------------------------------------
 --Table Utilisateur--
@@ -32,3 +32,20 @@ select * from Utilisateur join Ville using(vil_numero);
 
 --Test 2: Jointure Utilisateur et Message--
 select * from Utilisateur join Message using(uti_numero);
+
+-----------------------------------------------------------------------------------------
+--Nombres d'instruments:
+--en tout
+select count(*) from INSTRUMENT;
+
+--par type
+select count(*) from INSTRUMENT where MAR_NUMERO = 2 --Marque 2 = Yamaha
+
+--compter les marques différentes
+select count(MAR_NUMERO) as nb_marques from INSTRUMENT 
+where MAR_NUMERO is not null group by(MAR_NUMERO); -- 2 Marque 2, 2 Marque 1, reste à 1
+
+
+-- Recherche le nom dont le prix de location est différent de zéro
+select upper(INS_NOM) ins_nom, round(avg(INS_PRIX),2) prix_moy from INSTRUMENT 
+where INS_NOM is not null group by upper(INS_NOM) having avg(INS_PRIX)>0;
