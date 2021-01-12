f977e4d6a2510bfc8f4530df15501831efd9d764
Kilian Pichard <kil.pichard@icloud.com>
2021-01-12 10:26:42 +0100

    Modifications and add of annonces page

--- BDD.php
+++ BDD.php
@@ -3,15 +3,15 @@ include("pdo_oracle.php");
   function Connexion(){
     $login = 'instruments';
     $mdp = 'Esha2ohCheu5eij3';
-
     $db = 'mysql:host=localhost;dbname=instruments_bd';
     return OuvrirConnexionPDO($db,$login,$mdp);
   }
-
   if($_REQUEST["function"]=="cat"){
     getCat();
   }else if($_REQUEST["function"]=="new"){
     getNew();
+  }else if($_REQUEST["function"] == "famille"){
+    getFamille();
   }
 
   function sql($req){
@@ -35,16 +35,29 @@ function getCat(){
     }
   }
 }
+function getFamille(){
+  $req = 'SELECT DISTINCT CAT_FAMILLE FROM `CATEGORIE` order by CAT_FAMILLE';
+  $cat = sql($req);
+  for ($i=0;$i<sizeof($cat);$i++)
+  {
+    if($i == sizeof($cat)-1){
+      echo utf8_encode($cat[$i]['CAT_FAMILLE']);
+    }else{
+      echo utf8_encode($cat[$i]['CAT_FAMILLE'].'|');
+    }
+  }
+}
 
 function getNew(){
   $req = 'SELECT MARQUE.MAR_NOM, UTILISATEUR.UTI_NOM,UTILISATEUR.UTI_PRENOM,VILLE.VIL_NOM, INS_NOM, INS_DESCRIPTION,INS_PHOTO,UTI_PHOTO FROM `INSTRUMENT` as instru
 join MARQUE using (MAR_NUMERO)
 join VILLE using (VIL_NUMERO)
-join UTILISATEUR USING (UTI_NUMERO)';
+join UTILISATEUR USING (UTI_NUMERO)
+order by IN_DATE_AJOUT desc';
   $new = sql($req);
-  for ($i=0;$i<sizeof($new);$i++){
+  for ($i=0;$i<3;$i++){
     echo($new[$i]["MAR_NOM"].'|'.$new[$i]["UTI_NOM"].'|'.$new[$i]["UTI_PRENOM"].'|'.$new[$i]["VIL_NOM"].'|'.$new[$i]["INS_NOM"]
-      .'|'.utf8_encode($new[$i]["INS_DESCRIPTION"]).'|'.$new[$i]["INS_PHOTO"].'|'.$new[$i]["UTI_PHOTO"].'/////');
+      .'|'.utf8_encode($new[$i]["INS_DESCRIPTION"]).'|'.$new[$i]["INS_PHOTO"].'|'.$new[$i]["UTI_PHOTO"].'/separation/');
   }
 
 }
--- recherche.php
+++ recherche.php
@@ -19,19 +19,20 @@
   }
 
 function get(){
-  $req = 'SELECT MARQUE.MAR_NOM, UTILISATEUR.UTI_NOM,UTILISATEUR.UTI_PRENOM,VILLE.VIL_NOM, INS_NOM, INS_DESCRIPTION,INS_PHOTO,UTI_PHOTO,CAT_FAMILLE FROM `INSTRUMENT` as instru
+  $req = 'SELECT MARQUE.MAR_NOM, UTILISATEUR.UTI_NOM,UTILISATEUR.UTI_PRENOM,VILLE.VIL_NOM,VILLE.VIL_CP, INS_NOM, INS_DESCRIPTION,INS_PHOTO,UTI_PHOTO,CAT_FAMILLE FROM `INSTRUMENT` as instru
   join MARQUE using (MAR_NUMERO)
   join VILLE using (VIL_NUMERO)
   join UTILISATEUR USING (UTI_NUMERO)
-  join CATEGORIE using (CAT_NUMERO)';
+  join CATEGORIE using (CAT_NUMERO)
+  ORDER BY IN_DATE_AJOUT desc';
   send($req);
 }
 
   function send($req){
     $new = sql($req);
-    for ($i=0;$i<sizeof($new);$i++){
-      echo($new[$i]["MAR_NOM"].'|'.$new[$i]["UTI_NOM"].'|'.$new[$i]["UTI_PRENOM"].'|'.$new[$i]["VIL_NOM"].'|'.$new[$i]["INS_NOM"]
-        .'|'.utf8_encode($new[$i]["INS_DESCRIPTION"]).'|'.$new[$i]["INS_PHOTO"].'|'.$new[$i]["UTI_PHOTO"].'|'.$new[$i]["CAT_FAMILLE"].'//||//');
+    for ($i=0;$i<sizeof($new);$i++) {
+      echo(utf8_encode($new[$i]["MAR_NOM"] . '|' . $new[$i]["UTI_NOM"] . '|' . $new[$i]["UTI_PRENOM"] . '|' . $new[$i]["VIL_NOM"] . '|' . $new[$i]["INS_NOM"]
+        . '|' . $new[$i]["INS_DESCRIPTION"] . '|' . $new[$i]["INS_PHOTO"] . '|' . $new[$i]["UTI_PHOTO"] . '|' . $new[$i]["CAT_FAMILLE"] . '|' . $new[$i]["VIL_CP"] . '//||//'));
     }
   }
 ?>
