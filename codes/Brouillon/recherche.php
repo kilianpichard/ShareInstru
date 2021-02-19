13306a91fe3d2c01a0cc7ab0d36bb7c2c70638dc
Mathias Romero <damias22@live.fr>
2021-02-19 20:06:10 +0100

    Ajout exemple auto complétion utilisant l'api geo.api.gouv.fr

---
+++ codes/Brouillon/recherche.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.7.3/themes/base/jquery-ui.css">
+
+    </head>
+    <body>
+
+       <input name="ville" id="ville" type="text" placeholder="Ville">
+<script>
+$("#ville").autocomplete({
+    source: function (request, response) {
+        $.ajax({
+            url: "https://api-adresse.data.gouv.fr/search/?city="+$("input[name='ville']").val(),
+            data: { q: request.term },
+            dataType: "json",
+            success: function (data) {
+                var cities = [];
+                response($.map(data.features, function (item) {
+                    // Ici on est obligé d'ajouter les villes dans un array pour ne pas avoir plusieurs fois la même
+                    if ($.inArray(item.properties.postcode, cities) == -1) {
+                        cities.push(item.properties.postcode);
+                        return { label: item.properties.postcode + " - " + item.properties.city, 
+                                 postcode: item.properties.postcode,
+                                 value: item.properties.city
+                        };
+                    }
+                }));
+            }
+        });
+    },
+    // On remplit aussi le CP
+    select: function(event, ui) {
+        $('#cp').val(ui.item.postcode);
+    }
+});
+</script>
+
+    </body>
+</html>
